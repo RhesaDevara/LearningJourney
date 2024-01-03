dba6c0ec6a3cee1bff1790104197f67cc294307a
Rhesa Devara Widyaputra <117589401+RhesaDevara@users.noreply.github.com>
2024-01-03 17:32:10 +0700

    Initial commit

---
+++ app/src/main/java/com/reshproject/learningjourney/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.reshproject.learningjourney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
